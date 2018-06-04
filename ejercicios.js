b3c91ce34da893629f3511b397da2c69d66c2ec9
Victor Manuel Rivas Santos <vrivas@ujaen.es>
2018-06-04 19:29:21 +0200

    Segundo ejercicio

--- ejercicios.js
+++ ejercicios.js
@@ -1,3 +1,10 @@
+var palabras=["mama", "la cama", "papa"];
+var imagenes={
+  "mama": "https://www.wikihow.com/images/thumb/5/59/Deal-With-Baby-Mama-Drama-Step-1-Version-2.jpg/aid466158-v4-728px-Deal-With-Baby-Mama-Drama-Step-1-Version-2.jpg"
+  , "la cama": "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fimg-c.magazineluiza.com.br%2F1500x1500%2Fcama-solteiromoveis-boso-styllus-123273998.jpg&f=1"
+  , "papa":
+  "http://www.es.clipproject.info/images/joomgallery/details/personas_dibujos_29/hombre_dibujo_personas_dibujos_gente_7_20110102_1755468918.gif"
+}
 var ejercicios=[];
 
 function comprueba(respuesta, correcto) {
@@ -5,6 +12,8 @@ function comprueba(respuesta, correcto) {
   sonido.type="audio/mpeg"
   sonido.src=(respuesta==correcto)?"./correct-victory.mp3":"error-fallo-1.mp3";
   sonido.play();
+
+  if( respuesta==correcto ) setTimeout("haz_ejercicios();", 5000 );
 }
 
 function Fotos_3_Palabras_1( f1,f2,f3,p,c) {
@@ -32,17 +41,28 @@ function Fotos_3_Palabras_1( f1,f2,f3,p,c) {
   }
 }
 
+var contEjercicios=0;
 
 function haz_ejercicios() {
-  ejercicios[0].show();
+  ejercicios[contEjercicios++].show();
 }
 
 ejercicios.push(
   new Fotos_3_Palabras_1(
-    "https://www.wikihow.com/images/thumb/5/59/Deal-With-Baby-Mama-Drama-Step-1-Version-2.jpg/aid466158-v4-728px-Deal-With-Baby-Mama-Drama-Step-1-Version-2.jpg"
-    , "http://www.es.clipproject.info/images/joomgallery/details/personas_dibujos_29/hombre_dibujo_personas_dibujos_gente_7_20110102_1755468918.gif"
-    , "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fimg-c.magazineluiza.com.br%2F1500x1500%2Fcama-solteiromoveis-boso-styllus-123273998.jpg&f=1"
+    imagenes["mama"]
+    , imagenes["papa"]
+    , imagenes["la cama"]
     , "mamá"
     , 1
+  )
+);
+
+ejercicios.push(
+  new Fotos_3_Palabras_1(
+    imagenes["papa"]
+    , imagenes["la cama"]
+    , imagenes["mama"]
+    , "la cama"
+    , 2
   )
 );
